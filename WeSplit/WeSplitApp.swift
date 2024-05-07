ef2779ff08848284f98bf730a2dfc5b2af0158f1
Adriana Nieves <84536824+itsmeadriana@users.noreply.github.com>
2024-05-07 17:47:23 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Adriana Nieves on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
